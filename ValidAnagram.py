df98bd2102dd822b5f52443a8866b1fb96f5aa39
risingstark <seungkyu.sean.kim@gmail.com>
2022-08-17 15:42:50 +0900

    new solution is added

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,11 +1,13 @@
 # Given two strings s1 and s2, checn if they are anagrams. 
 # Two strings ar anagrams if they are made of the same characters 
 # with the same frequencies
+from collections import Counter
 
 class Solution:
 
     # solution 1. 
-    # frequency1 = frequency2    
+    # frequency1 = frequency2
+    # Time complexity : O(N), Space : O(N)
     def validAnagram1(self,s1,s2):
         ''' (self, string, string)
         return True if given s1 and s2 are anagrams, 
@@ -15,9 +17,12 @@ class Solution:
             return False
 
         freq1 = self.helpBuildFreq(s1,{})
-        freq2 = self.helpBuildFreq(s1,{})
+        freq2 = self.helpBuildFreq(s2,{})
 
-        return freq1==freq2
+        for key in freq1:
+            if key not in freq1 or freq1[key] != freq2[key]:
+                return False
+        return True
 
     def helpBuildFreq(self,word,fdict):
         for s in word:
@@ -27,6 +32,16 @@ class Solution:
                 fdict[s]+=1
         return fdict
 
+    # solution2
+    # using sorted(), which is built-in, using TimSort(); combine mergesort and selectionsort
+    # sorted() takes O(nlogn) and space space(N)
+    def validAnagram2(self,s1,s2):
+        
+        if len(s1) != len(s2):
+            return False
+        
+        return sorted(s1) == sorted(s2)
+
 if __name__ == "__main__":
 
     s1 = "danger"
@@ -34,5 +49,6 @@ if __name__ == "__main__":
     s = Solution()
     print(s.validAnagram1(s1,s2))
 
-    s1 = "nameless"
-    s2 = "salesman"
+    s1 = "debit card"
+    s2 = "bad credit"
+    print(s.validAnagram2(s1,s2))
