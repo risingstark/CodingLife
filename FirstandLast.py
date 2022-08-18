31690da60e4bc2121b144fa410ae48bbd262e4e0
risingstark <seungkyu.sean.kim@gmail.com>
2022-08-18 14:40:58 +0900

    update first solution

--- FirstandLast.py
+++ FirstandLast.py
@@ -8,24 +8,34 @@
 class Solution:
     
     # soluotion 1
-    # using for loop
-    def firstandlast(self,arr,target):
+    # using for loop, time: O(N), space: O(1)
+    def firstandlast1(self,arr,target):
         
         res = []
         for i,v in enumerate(arr):
             if v == target and len(res)==0:
                 res.append(i)
-            elif v !=target and len(res) ==1:
+                while i <= len(arr)-1 and arr[i] == target:
+                    i+=1
                 res.append(i-1)
-                break
-            elif v ==target and i == len(arr)-1:
-                res.append(i)
+                return res
+        return [-1,-1]
+        
+
+
+    # solution 2
+    # using binary search
+    def firstandlast2(self,arr,target):
+
+        res = []
+
+
         return res
 
 if __name__=="__main__":
-    
-    arr = [2,4,5,5,5,5,5,7,9,9]
+
+    arr = [2,4,5,5,5,5,5]
     target = 5
     output = [2,6]
     s = Solution()
-    print(s.firstandlast(arr,target) == output)
+    print(s.firstandlast1(arr,target) == output)
