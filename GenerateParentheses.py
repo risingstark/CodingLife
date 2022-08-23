5aa83a1c0e363854d64db8a7f922e5933a4da594
risingstark <seungkyu.sean.kim@gmail.com>
2022-08-23 11:52:45 +0900

    new solution

---
+++ GenerateParentheses.py
@@ -0,0 +1,27 @@
+# Givcen an integer n, generate all the valid
+# combinations of n pairs of parentheses
+
+class Solution():
+    def generate(self,n):
+        def rec(n,diff,comb,combs):
+            if diff < 0 or n < 0:
+                return
+            elif n == 0 and diff == 0:
+                return combs.append(''.join(comb))
+            else:
+                comb.append('(')
+                rec(n-1,diff+1,comb,combs)
+                comb.pop()
+                comb.append(')')
+                rec(n-1,diff-1,comb,combs)
+                comb.pop()
+        
+        combs = []
+        rec(2*n,0,[],combs)
+        return combs
+
+
+if __name__=="__main__":
+    s = Solution()
+    n = 2
+    print(s.generate(n))
