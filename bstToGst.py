46d95ca3c448e5a3f5089a5236b0b9612386eac5
risingstark <seungkyu.sean.kim@gmail.com>
2022-09-12 10:25:39 +0900

    bst to greater sum tree is added

---
+++ bstToGst.py
@@ -0,0 +1,75 @@
+# Binary Search Tree to Greater Sum Tree
+
+# Given the root of a Binary Search Tree (BST), convert it to a Greater Tree 
+# such that every key of the original BST is changed to the original key plus 
+# the sum of all keys greater than the original key in BST.
+
+# As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+ 
+# Example 1:
+# Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+# Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+# Example 2:
+# Input: root = [0,null,1]
+# Output: [1,null,1]
+ 
+
+# Constraints:
+# The number of nodes in the tree is in the range [1, 100].
+# 0 <= Node.val <= 100
+# All the values in the tree are unique.
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    
+    # time: O(n) , space: O(n)
+    def bstToGst1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        # iterately 
+        if not root:
+            return None
+        v_sum = 0
+        node = root
+        stack = []
+        
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.right
+            node = stack.pop()
+            v_sum += node.val
+            node.val = v_sum
+            node = node.left
+        return root
+        
+    # time: O(n), space: O(n)
+    def bstToGst1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+        # recursively
+        def reversedInorder(node, sm):
+            if node:
+                node.val += reversedInorder(node.right, sm)
+                return reversedInorder(node.left, node.val)
+            return sm
+
+        reversedInorder(root, 0)
+        return root
